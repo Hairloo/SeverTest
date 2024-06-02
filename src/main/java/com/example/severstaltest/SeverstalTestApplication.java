a3fef3577fec8d0fb145529015963ff320ffbb3d
wannacry <lobanov4747@gmail.com>
2024-06-02 19:15:31 +0300

    Создан проект SeverstalTest

---
+++ src/main/java/com/example/severstaltest/SeverstalTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.severstaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverstalTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeverstalTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/severstaltest/SeverstalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.severstaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeverstalTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
